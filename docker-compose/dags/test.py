94ef6b1ce81001c9c7766242c2e6cc89e5f5ab1b
prasadgugulla <prasadgugulla9@gmail.com>
2023-02-10 10:55:01 +0000

    new dag

---
+++ docker-compose/dags/test.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print('Hello from IBM Cloud Airflow')
+
+with DAG(dag_id= 'hello',
+         start_date= datetime(2023,2,10),
+         schedule_interval='@hourly',
+         catchup=False) as dag:
+         task1 = PythonOperator(task_id ='hello',
+         python_callable=hello)
+         
+task1
