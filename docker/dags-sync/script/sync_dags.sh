89366eb99b0d182e5be66929a31323acfcb5238c
pkwame <45004664+pkwame@users.noreply.github.com>
2023-02-15 14:10:05 +0000

    Working sync script

--- docker/dags-sync/script/sync_dags.sh
+++ docker/dags-sync/script/sync_dags.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 airflow_root=`pwd`
-# pod_name=$(oc get pods --selector io.kompose.service=scheduler --output=name)
 pod_name=$(oc get pods --selector io.kompose.service=scheduler --output=name | sed -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//' -e 's/^[[pod/]]*//')
 reponame="https://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@github.com/FD-Data-Engineering/sf-airflow.git"
-airflow_dir="sf-airflow/docker-compose"
+airflow_dir="sf-airflow/airflow-folders"
 git_dir=git
 pod_dest="../../../"
 
-oc exec $pod_name bash -c "echo $pod_name && \
-        mkdir git && \
-        cd git && \
-        git init && \
-        git config credential.helper store && \
-        git clone $reponame && \
-        cd $airflow_dir && \
-        cp -r dags $pod_dest && \
-        cd $airflow_root && \
-airflow scheduler -D "
-
+oc exec $pod_name -- bash -c '
+        pwd
+        rm -rf '$git_dir' && mkdir git
+        cd git
+        git init
+        git config credential.helper store
+        git clone '$reponame'
+        cd '$airflow_dir'
+        cp -r dags '$pod_dest'
+        cd '$airflow_root'
+airflow scheduler -D '
