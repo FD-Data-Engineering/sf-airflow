197fcbfcff025377956b98c74fb419abb0e26d0e
prasadgugulla <prasadgugulla9@gmail.com>
2023-05-12 16:51:20 +0100

    dag_sf_airbus_data.py updated

--- airflow-folders/dags/dag_sf_airbus_data.py
+++ airflow-folders/dags/dag_sf_airbus_data.py
@@ -88,8 +88,8 @@ mergeairbus = PythonOperator(task_id='mergeairbus', python_callable=triggerBatch
         "application": "cos://transformedairbusdata.Airbus/scripts/v.1.0/mergeairbus.py",
         "arguments": ["cos://ingestedairbusdata/portfolio.Airbus/dt="+dtStr+"/01_NBS_Portfolio_Bristol_230216.csv","cos://ingestedairbusdata/twinn.Airbus/dt="+dtStr+"/02_NBS_Portfolio_Bristol_230216_TWINN.csv","cos://ingestedairbusdata/latlong.Airbus/dt="+dtStr+"/03_NBS_Portfolio_Bristol_230216_LatLon.csv"],
         "conf": {"spark.hadoop.fs.cos.Airbus.endpoint": "s3.direct.eu-de.cloud-object-storage.appdomain.cloud",
-                 "spark.hadoop.fs.cos.Airbus.access.key": "01fc0d80849541eda6515b9d6ea2329b",
-                 "spark.hadoop.fs.cos.Airbus.secret.key": "65862b4d74e5183f18f96bed6b44c93f235ea3363bf6d607",
+                 "spark.hadoop.fs.cos.Airbus.access.key": "3dbafb4b2fc74197bf8502767a9a73cc",
+                 "spark.hadoop.fs.cos.Airbus.secret.key": "5c306426caedaf8782fd4cd8a1cf39e6e46e36e20e885f7a",
                  "spark.app.name": "SF-Airbus-Merge-data"      }   }  }}, dag=dag)
 
 checkMergeAirbusStatus = PythonOperator(task_id='mergeairbusJobStatus', python_callable=jobCompletionCheck, op_kwargs={"api_url":"https://api.eu-de.ae.cloud.ibm.com/v3/analytics_engines/2f641c08-2aee-438c-b8a0-eb1738f88c58/spark_applications/{{ task_instance.xcom_pull(task_ids='mergeairbus') }}/state","access_token":Variable.get("Access_Token")}, dag=dag)   
