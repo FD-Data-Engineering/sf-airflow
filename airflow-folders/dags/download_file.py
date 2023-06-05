9a178e23b32f6c5d328befdcf30eceda684b33b7
prasadgugulla <prasadgugulla9@gmail.com>
2023-06-05 11:13:44 +0100

    new sample dag added

--- airflow-folders/dags/download_file.py
+++ airflow-folders/dags/download_file.py
@@ -30,7 +30,7 @@ start = DummyOperator(task_id="start", dag=dag)
 
 File_Download = BashOperator(
     task_id="Download_File",
-    bash_command="curl --retry 5 -O /usr/local/airflow/downloads/os-climate-diagrams.pdf https://github.com/os-climate/os_c_data_commons/blob/main/os-climate-diagrams.pdf",
+    bash_command="curl https://github.com/os-climate/os_c_data_commons/blob/main/os-climate-diagrams.pdf  -O /usr/local/airflow/downloads/os-climate-diagrams.pdf --retry 2",
     dag=dag
 )
 
