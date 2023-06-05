f1225ed9d773d83451cb249d68ace92af43838bb
prasadgugulla <prasadgugulla9@gmail.com>
2023-06-05 10:20:03 +0100

    new sample dag added

---
+++ airflow-folders/dags/download_file.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+now = datetime.now()
+print("File Downloaded")
+#FILE = "/usr/local/downloads/NAWS_A2E197.csv"
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(now.year, now.month, now.day),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1)
+}
+
+dag = DAG(
+        dag_id="Download_file_from_local", 
+        description="This DAG downloads a file from local and then sucess else failure if file doesn't exists",
+        default_args=default_args, 
+        schedule_interval=timedelta(1)
+    )
+start = DummyOperator(task_id="start", dag=dag)
+
+
+File_Download = BashOperator(
+    task_id="Download_File",
+    bash_command="curl --retry 5 -O -C - C:\Users\pgugulla\prometheus",
+    dag=dag,
+)
+
+end = DummyOperator(task_id="end", dag=dag)  
+
+start >> File_Download >> end
+#testing 
